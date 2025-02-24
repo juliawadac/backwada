6adb76fbbaa91af7f5c278a09fcf93b041498945
wada <juliawadac@gmail.com>
2025-02-24 17:09:47 -0300

    Update userService.js

--- aula 3/userService.js
+++ aula 3/userService.js
@@ -1,10 +1,37 @@
 const user = require ("./user");
-
+const path = require ('path');
+const fs = require ('fs');
 
 class userservice {
     constructor () {
+        this.filepath = path.join(_dirname, 'user.json');
         this.users = []; // array para armazenar user 
         this.nextid = 1; // contador para gerar id
+    }
+
+    loadusers () {
+        try{
+        if (fs.existsSync (this.filepath)) { // verifica se o arquivo existe
+            const data =fs.readFileSync (this.filepath); // le o arquivo
+            return json.parse (data); //transforma json em objeto - array
+        }
+    }catch (erro){
+        console.log ('erro ao carregar arquivo', erro);
+    }
+        return []; //retorna um array vazio     
+    }
+
+
+    //definir o proximo id a ser utilizado
+
+    getnextid (){
+        try {
+            if (this.users.length===0) return 1;
+        return Math.max (...this.users.map(user => user.id))+1;
+        }
+        catch (erro) {
+            console.log ('erro ao buscar proximo id', erro);
+        }
     }
 
     adduser (nome,email) {
