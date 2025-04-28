44ee8bc960fa8011ca079a84d2c860340fa0fdb2
juliawadac <juliawadac@gmai.com>
2025-04-28 16:26:21 -0300

    config projeto api

---
+++ api-hopihari/index.js
@@ -0,0 +1,23 @@
+const express = require ("express");
+const cors = require ("cors");
+const app = express();
+const bodyParser = require ("body-parser");
+const helmet = require ("helmet");
+
+app.use (cors());
+app.use (helmet());
+
+app.use (bodyParser.urlencoded({extended:false}));
+app.use (bodyParser.json());
+
+app.use ((req, res, next) => {
+    res.header("Access-Control-Allow-Origin0", "*");
+    res.header (
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-type, accept, Autorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
+    }
+    next();
+})
